b111cf0287a310534380a568e7e6760a324da654
Nide17 <parmenin@andrew.cmu.edu>
2024-04-01 13:09:36 +0200

    sockets

--- utils/socket.js
+++ utils/socket.js
@@ -14,31 +14,16 @@ module.exports = {
         io.on('connection', (socket) => {
             console.log('New client connected with id = ', socket.id);
 
-            // Listen for 'userConnected' event
-            socket.on('userConnected', (userId) => {
-                console.log('User connected with id = ', userId);
+            // Listen for 'newUserConnected' event
+            socket.on('newUserConnected', ({ _id, name, email }) => {
+
+                console.log("New user connected: ", name, email, _id);
+
+                // Update the list of users when a new user connects to the server
+                onlineUsers.push({ socketID: socket.id, _id, name, email });
+                io.emit('newUserOnline', onlineUsers);
             });
 
-            // Receiving message from the client
-            socket.on('frontJoinedUser', ({ user_id, email, username, role }) => {
-
-                onlineUsers.push({ socketID: socket.id, user_id, email, username, role });
-                console.log("Joined user:")
-
-                console.log(JSON.stringify({ user_id, email, username, role }));
-                console.log('Online users:' + onlineUsers.length);
-
-                console.log(JSON.stringify(onlineUsers))
-
-                // Sending message from the server to all the clients except the current user
-                socket.broadcast.emit('backJoinedUser', onlineUsers[onlineUsers.length - 1]);
-
-                console.log('Joined last:');
-                console.log(JSON.stringify(onlineUsers[onlineUsers.length - 1]))
-
-                // Sending a list of online users to all the clients including sender
-                io.emit('onlineUsersList', onlineUsers);
-            });
 
             // Catch the emitted message from client
             socket.on('contactMsgClient', (contactMsg) => {
@@ -79,21 +64,16 @@ module.exports = {
 
             });
 
-            // Listen for 'userDisconnected' event
-            socket.on('userDisconnected', (userId) => {
-                console.log('User disconnected with id = ', userId);
-            });
+
+
 
             // on disconnect
             socket.on('disconnect', (reason) => {
                 console.log('A client disconnected with id = ', socket.id, " reason ==> ", reason);
 
-
-                //Updates the list of users when a user disconnects from the server
-                onlineUsers = onlineUsers.filter((user) => user.socketID !== socket.id);
-
-                //Sends the list of users to the client
-                io.emit('onlineUsersList', onlineUsers);
+                // Remove user from online list
+                onlineUsers = onlineUsers.filter(user => user.socketID !== socket.id);
+                io.emit('userOffline', onlineUsers);
             });
         });
     },
